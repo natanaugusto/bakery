efdede76069173271651a493ab70a904833ffafd
Natan Augusto <natanaugusto@gmail.com>
2022-03-29 18:21:50 -0300

    Created the initial migrations to the project - https://laravel.com/docs/9.x/migrations

---
+++ database/migrations/2022_03_25_230845_create_sale_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sale_product', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId(column: 'product_id');
+            $table->foreignId(column: 'sale_id');
+            $table->boolean('is_ready');
+            $table->unsignedMediumInteger(column: 'amount');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sale_product');
+    }
+};
